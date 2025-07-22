3324599186c9199b1543f050c75d28fe44000d9f
jasonrn36 <jason.rn36@gmail.com>
2025-07-22 16:36:17 -0400

    finalizando tarefas do git

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,23 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-
-function compilaSass() {
-    return gulp.src('./source/styles/main.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./build/styles'));
-}
+const gulp = require('gulp'); 
 
 function funcaoPadrao(callback) {
     setTimeout(function(){
-        console.log("\n\x1b[33mEssa tarefa só inicia apos 4 segundos\x1b[37m \n");
+        console.log("\n\x1b[33mEssa é a primeira tarefa função padrão\x1b[37m \n");
         callback();
 
-    }, 3992);
+    }, 991);
 }
 
 function digaOi(callback) {
         setTimeout(function(){
-    console.log("\n\x1b[0m\x1b[33mEssa é a segunda tarefa junto com a primeira\x1b[37m");
+    console.log("\n\x1b[0m\x1b[33mEssa é a segunda tarefa Dizendo Oi\x1b[37m");
     digaTchau();
     callback();
-    }, 991);
+    }, 3992);
 
 }
 function digaTchau(){
-    console.log("\x1b[0m\x1b[33m Essa é a terceira tarefa junto com a segunda\x1b[37m \n");
+    console.log("\x1b[0m\x1b[33m Essa é a terceira tarefa Dizendo tchau!\x1b[37m \n");
 }
 
-exports.default = gulp.parallel (funcaoPadrao, digaOi);
-exports.digaOi = digaOi;
-
-exports.sass = compilaSass;
+exports.default = gulp.parallel(funcaoPadrao,digaOi);
